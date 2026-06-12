6ccb5b7a54adfc61a60163f93bb00fecaa865d55
Arman Singh <armanprabhat123912@gmail.com>
2026-06-12 23:25:31 +0530

    chore: setup deployment files for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Using './' forces all assets to load relative to the index.html file
-  // This solves 99% of 404s on GitHub Pages
-  base: './',
+  // This tells Vite that all your files start at /exigo-tech-/
+  base: '/exigo-tech-/',
 })
